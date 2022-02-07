a862b5716e504cbb3122341135230dd241e6eed9
ionel2988 <ionel2988@gmail.com>
2022-02-08 00:25:17 +0800

    get nft info from api

--- services/api.ts
+++ services/api.ts
@@ -13,14 +13,14 @@ class ApiService {
 
   public async get(token_id: number | string) {
     const { data } = await axios.get(baseUrl + "tokens/" + token_id);
-    const url = `http://198.58.105.159:9094/api/rwalk/tokens/history/${token_id}/0x895a6F444BE4ba9d124F61DF736605792B35D66b/0/1000`;
+    const url = `https://198.58.105.159:9094/api/rwalk/tokens/history/${token_id}/0x895a6F444BE4ba9d124F61DF736605792B35D66b/0/1000`;
     const res = await axios.get(url);
     data.tokenHistory = res?.data?.TokenHistory;
     return data;
   }
 
   public async get_info(token_id: number | string) {
-    const url = `http://198.58.105.159:9094/api/rwalk/tokens/info/0x895a6F444BE4ba9d124F61DF736605792B35D66b/${token_id}`;
+    const url = `https://198.58.105.159:9094/api/rwalk/tokens/info/0x895a6F444BE4ba9d124F61DF736605792B35D66b/${token_id}`;
     const { data } = await axios.get(url);
     return data;
   }
