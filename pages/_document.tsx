b319162bcb1b02145027e0611d311e3c10c404a2
ionel2988 <ionel2988@gmail.com>
2022-03-21 15:02:41 +0000

    fix issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,10 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width,initial-scale=1" />
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta
+            name="google-site-verification"
+            content="ZUw5gzqw7CFIEZgCJ2pLy-MhDe7Fdotpc31fS75v3dE"
+          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
