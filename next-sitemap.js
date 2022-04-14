cc771cba106f2cbc8a93d383d15ea69a93a4099d
ionel2988 <ionel2988@gmail.com>
2022-04-15 06:19:22 +0800

    fix issue

---
+++ next-sitemap.js
@@ -0,0 +1,25 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL,
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "test-bot",
+        allow: ["/path", "/path-2"],
+      },
+      {
+        userAgent: "black-listed-bot",
+        disallow: ["/sub-path-1", "/path-2"],
+      },
+    ],
+  },
+};
