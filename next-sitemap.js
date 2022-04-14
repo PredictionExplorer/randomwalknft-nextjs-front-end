57bb905d27c8f43a9c7f0ff090d69a6398461812
ionel2988 <ionel2988@gmail.com>
2022-04-15 06:31:10 +0800

    fix issue

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL,
+  siteUrl: "https://www.randomwalknft.com/",
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 5000,
