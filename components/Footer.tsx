bd2f9b1d6d46c2818b5f4812e2909fe0119591e8
ionel2988 <ionel2988@gmail.com>
2024-07-08 08:42:53 -0400

    remove text 'member of Arbitrum NFT alliance' from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,26 +19,6 @@ const Footer = () => (
     <Toolbar>
       <Container maxWidth="lg">
         <Box py={1} display="flex" justifyContent="center" alignItems="center">
-          <Box mr={2}>
-            <Typography variant="body2" component="p">
-              <Typography
-                variant="body2"
-                component="span"
-                color="textSecondary"
-              >
-                Member of the
-              </Typography>
-              &nbsp;
-              <Link
-                color="textSecondary"
-                target="_blank"
-                href={`https://www.nftalliance.xyz`}
-                style={{ textDecoration: 'underline' }}
-              >
-                Arbitrum NFT Alliance
-              </Link>
-            </Typography>
-          </Box>
           <IconButton href="https://twitter.com/RandomWalkNFT" target="_blank">
             <FontAwesomeIcon icon={faTwitter} size="xs" color="#C676D7" />
           </IconButton>
